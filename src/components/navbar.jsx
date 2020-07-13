10017283df60f9a3bf29bab5333649f2b61e33fe
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-12 20:35:31 -0400

    carousel

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -20,18 +20,18 @@ const Navbar = props => {
         <div
             className="collapse navbar-collapse mr-5"
             id="navbarNavDropdown">
-            <ul className="navbar-nav ml-auto">
+            <ul className="navbar-nav ml-auto border-right">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
+                    <Link className="nav-link " to="/">Home<span className="sr-only">(current)</span></Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/cv">CV</Link>
+                    <Link className="nav-link" to="/register">Registrarme</Link>
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" to="/services">Servicios</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/projects">Projectos</Link>
+                    <Link className="nav-link border" to="/projects">Projectos</Link>
                 </li>
                 {/* <li className="nav-item">
                     <Link className="nav-link" to="/contact">Contacto</Link>
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,40 +1,83 @@
 import React from "react"
 
 const Home = props => {
-    return ( 
+    return (
         <div>
-        <h1>Home</h1>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
-        <h2>a</h2>
+            {/* banner */}
+            <div id="banner">
+                <div id="carouselExampleCaptions" className="carousel slide " data-ride="carousel" >
+                    {/* <ol className="carousel-indicators" >
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol> */}
+                    <div className="carousel-inner" id="carrusel-1" role="listbox">
+                        <div className="carousel-item active" id="carrusel-3">
+                            <img src="https://images.radio.com/aiu-media/GettyImages517051420-715fc146-7908-4a7e-b481-2f6602c3297b.jpg?width=800"
+                                className="d-block w-100"
+                                height="500px"
+                                alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h1>Busca a tu médico</h1>
+                            </div>
+                        </div>
+                        <div className="carousel-item" id="carrusel-3">
+                            <img src="https://www.socialblabla.com/wp-content/uploads/2016/10/calendario-redes-sociales.jpg"
+                                className="d-block w-100"
+                                height="500px"
+                                alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h1>Agenda una hora</h1>
+                            </div>
+                        </div>
+
+                        <div className="carousel-item " id="carrusel-2">
+                            <img
+                                src="https://thumbs.dreamstime.com/b/clasificaci%C3%B3n-de-m%C3%A9dicos-concepto-cinco-estrellas-buscar-buen-doctor-el-personal-m%C3%A9dico-examina-la-ilustraci%C3%B3n-vectorial-164214469.jpg"
+                                className="d-block w-100"
+                                height="500px"
+                                alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h1>Valora la atención </h1>
+                            </div>
+                        </div>
+
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+
+          
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
+            <h2>a</h2>
 
 
 
